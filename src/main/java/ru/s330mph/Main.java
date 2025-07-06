a96582cb911f519f7192dd32e92d836795d5e059
s330mph <s330mph@gmail.com>
2025-07-06 22:13:47 +0300

    added homework

--- src/main/java/ru/s330mph/Main.java
+++ src/main/java/ru/s330mph/Main.java
@@ -4,14 +4,35 @@ package ru.s330mph;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int x = 10;
+        int y = 20;
+        int z = -5;
+
+        int summ = x + y;
+        System.out.println(summ);
+
+        int diff = x - y;
+        System.out.println(diff);
+
+        int multiply = y*z;
+        System.out.println(multiply);
+
+        int div = y/z;
+        System.out.println(div);
+
+        double q = (7.7+x)*5/2;
+        System.out.println(q);
+
+        if (x < y && y > z) {
+            System.out.println("правда");
+        }else if (x < y || z > y){
+            System.out.println("правда");
+        }else {
+            System.out.println("ложь");
         }
+
+        int result = (int) Math.pow(2, 33);
+        System.out.println(result);
     }
 }
